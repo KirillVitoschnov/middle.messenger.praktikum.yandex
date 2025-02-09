8c6936d3c1cde0c14c180736f3d92319dc0b5031
Кирилл Витошнов <64968241+KirillVitoschnov@users.noreply.github.com>
2025-02-09 23:22:06 +0500

    feat: итерация по интеграцию апи

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -34,60 +34,7 @@ export class ChatController {
             });
     }
 
-    // Удалить чат по ID
-    public deleteChat(chatId: number) {
-        return chatAPI
-            .deleteChatAPI(chatId)
-            .then(() => {
-                this.getChats(); // Обновляем список чатов после удаления
-            })
-            .catch((error) => {
-                console.error('Ошибка при удалении чата:', error);
-                store.setState('errorMessage', JSON.parse(error.response).reason);
-            });
-    }
 
-    // Добавить пользователя в чат
-    public addUserToChat(chatId: number, userId: number) {
-        return chatAPI
-            .addUserToChatAPI({ chatId, userId })
-            .then(() => {
-            })
-            .catch((error) => {
-                console.error('Ошибка при добавлении пользователя в чат:', error);
-                store.setState('errorMessage', JSON.parse(error.response).reason);
-            });
-    }
-
-    // Удалить пользователя из чата
-    public removeUserFromChat(chatId: number, userId: number) {
-        return chatAPI
-            .removeUserFromChatAPI({ chatId, userId })
-            .then(() => {
-                console.log(`Пользователь ${userId} удалён из чата ${chatId}`);
-            })
-            .catch((error) => {
-                console.error('Ошибка при удалении пользователя из чата:', error);
-                store.setState('errorMessage', JSON.parse(error.response).reason);
-            });
-    }
-
-    // Получить сообщения чата по ID (если необходимо использовать HTTP-запросы)
-    public getChatMessages(chatId: number) {
-        return this.getChatToken(chatId)
-            .then((token) => {
-                return chatAPI
-                    .getChatMessagesAPI(chatId, token)
-                    .then((data) => {
-                        store.setState(`messages_${chatId}`, JSON.parse(data));
-                        return JSON.parse(data);
-                    });
-            })
-            .catch((error) => {
-                console.error('Ошибка при получении сообщений чата:', error);
-                store.setState('errorMessage', JSON.parse(error.response).reason);
-            });
-    }
 
     // Получить токен для чата
     public getChatToken(chatId: number) {
