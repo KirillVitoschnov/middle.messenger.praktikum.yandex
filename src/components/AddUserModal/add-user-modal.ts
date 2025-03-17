40733b45b3539d0775e53aa8f879dbdf44d10346
Кирилл Витошнов <64968241+KirillVitoschnov@users.noreply.github.com>
2025-03-17 23:31:45 +0500

    feat: м

--- src/components/AddUserModal/add-user-modal.ts
+++ src/components/AddUserModal/add-user-modal.ts
@@ -1,15 +1,13 @@
-import { Block } from '../../services';
-import { TProps } from '../../types';
-import template from './template.hbs?raw';
-import * as Component from '../index';
-import * as Service from '../../services';
-import {  getDataForm, isEqual } from '../../utils';
-import {  userController } from '../../controllers';
-import { store } from '../../store';
+import { Block } from '../../services'
+import { TProps } from '../../types'
+import template from './template.hbs?raw'
+import * as Component from '../index'
+import * as Service from '../../services'
+import { getDataForm, isEqual } from '../../utils'
+import { userController } from '../../controllers'
+import { store } from '../../store'
 
-export default class AddUserModal extends Block<TProps> {
-  private currentUsers = [];
-
+export class AddUserModal extends Block<TProps> {
   constructor(props: TProps) {
     const fieldsProps = [
       {
@@ -20,88 +18,91 @@ export default class AddUserModal extends Block<TProps> {
           value: '',
           attr: {
             'data-required': true,
-            'data-valid-login': true,
+            'data-valid-login': true
           },
           events: {
             blur: (event: FocusEvent) => {
-              Service.validate(event.target as HTMLInputElement);
-            },
-          },
-        }),
-      },
-    ];
+              Service.validate(event.target as HTMLInputElement)
+            }
+          }
+        })
+      }
+    ]
 
-    const inputBlocks = fieldsProps.map((field) => new Component.InputBlock(field));
+    const inputBlocks = fieldsProps.map((field) => new Component.InputBlock(field))
 
     const saveButton = new Component.Button({
       text: 'Поиск пользователя',
-      attr: { withInternalID: true, type: 'submit' },
-    });
+      attr: { withInternalID: true, type: 'submit' }
+    })
 
     const form = new Component.Form({
       inputBlocks,
       button: saveButton,
       events: {
         submit: async (event: Event) => {
-          event.preventDefault();
-          const formData = getDataForm(event);
+          event.preventDefault()
+          const formData = getDataForm(event)
           if (typeof formData.login === 'string') {
             try {
-              await userController.searchUser(formData.login);
-            } catch (error) {
-              console.error('Ошибка при поиске пользователя:', error);
-            }
-          } else {
-            console.error('Ошибка: логин не является строкой');
+              await userController.searchUser(formData.login)
+            } catch (error) {}
           }
-        },
-      },
-    });
+        }
+      }
+    })
 
-
-    const state = store.getState();
-    console.log(state.users)
+    const state = store.getState()
     const userInfoItems = new Component.UserInfoItems({
       items: (state.users || []).map((user) =>
-          new Component.UserInfoItem({
-            user:user
-          })
-      ),
-    });
-console.log(userInfoItems)
+          new Component.UserInfoItem({ user })
+      )
+    })
+
     super({
       ...props,
       form,
       userInfoItems,
       events: {
         click: (event: MouseEvent) => {
-          const target = event.target as HTMLElement;
+          const target = event.target as HTMLElement
           if (
               target.dataset.close === 'true' ||
               target.classList.contains('modal-overlay')
           ) {
-            this.setProps({ isOpen: false });
+            this.setProps({ isOpen: false })
           }
-        },
-      },
-    });
-    this.currentUsers = state.users || [];
+        }
+      }
+    })
   }
+
   override componentDidUpdate(oldProps: TProps, newProps: TProps): boolean {
-    console.log(oldProps, newProps);
     if (isEqual(oldProps, newProps)) return false
+    const state = store.getState()
+    const updatedUserInfoItems = new Component.UserInfoItems({
+      items: (state.users || []).map((user) =>
+          new Component.UserInfoItem({ user })
+      )
+    })
+    this.children.userInfoItems = updatedUserInfoItems
     return true
   }
 
   public render() {
-    const { isOpen } = this.props;
+    const { isOpen } = this.props
     const overlayClass = isOpen
         ? 'modal-overlay add-user-overlay'
-        : 'modal-overlay add-user-overlay modal-closed';
-
+        : 'modal-overlay add-user-overlay modal-closed'
     return this.compile(template, {
       ...this.props,
-      overlayClass,
-    });
+      overlayClass
+    })
   }
 }
+
+function mapStateToProps(state: any): Partial<TProps> {
+  return state
+}
+
+export default Service.connect(AddUserModal, mapStateToProps)
