2f5f900a0553c8f5541950003e0f0fff9eb0bb98
Кирилл Витошнов <64968241+KirillVitoschnov@users.noreply.github.com>
2025-04-06 00:33:27 +0500

    feat: prettier

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,15 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-    plugins: [
-        nodePolyfills(),
-    ],
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
             },
         },
-        commonjsOptions: {
-            transformMixedEsModules: true,
-        },
     },
     css: {
         postcss: "./postcss.config.cjs",
-    },
-    resolve: {
-        alias: {
-            crypto: 'crypto-browserify',
-        },
     },
 });
